fa1d5c3c25d392d28bd4ec27dcd2a0a85839a040
Nick Bolten <nbolten@gmail.com>
2016-02-11 19:43:30 -0800

    Removed old tile comments

--- public/javascripts/bundle.js
+++ public/javascripts/bundle.js
@@ -89,10 +89,6 @@ return /******/ (function(modules) { // webpackBootstrap
 	    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
 	  }).addTo(map);
 
-	  // var tiles = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.png', {
-	  //   attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-	  // }).addTo(map);
-
 	  var elevationTiles = L.mapbox.tileLayer(tile_url);
 	  elevationTiles.addTo(map);
 
