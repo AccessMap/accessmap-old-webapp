fcf4d802a48b6d494973b80bff66dda7e8ef93d5
Nick Bolten <nbolten@gmail.com>
2016-12-20 23:56:03 -0700

    Implemented construction `live` layer for things like construction, updated style

--- es6/map.js
+++ es6/map.js
@@ -8,8 +8,10 @@ import AccessMapGradeControl from './AccessMapGradeControl';
 
 
 function App(mapbox_token, routing) {
+  // Zoom point at which map starts by default
+  const zoomStart = 15;
   // Zoom point at which features (e.g. sidewalk) become clickable
-  const clickable = 16;
+  const clickable = 15;
 
   //
   // Styling
@@ -25,7 +27,7 @@ function App(mapbox_token, routing) {
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v8',
     center: [-122.333592, 47.605628],
-    zoom: 15
+    zoom: zoomStart
   });
 
   map.on('load', function() {
@@ -41,6 +43,7 @@ function App(mapbox_token, routing) {
         + (window.location.port ? ':' + window.location.port : '');
     }
     let tilesUrl = window.location.origin + '/tiles/seattle/{z}/{x}/{y}.mvt';
+    let liveUrl = window.location.origin + '/tiles/live/{z}/{x}/{y}.mvt';
 
     //
     // Data sources - used by layers to draw data
@@ -53,44 +56,61 @@ function App(mapbox_token, routing) {
       attribution: '&copy; AccessMap'
     });
 
+    map.addSource('live', {
+      type: 'vector',
+      tiles: [liveUrl]
+    });
+
+    // live
+    // FIXME: tried using 'symbol' layer with roadblock-15 sprite, but it
+    //        wouldn't render (though other symbols would)
+    map.addLayer({
+      id: 'construction-outline',
+      type: 'circle',
+      source: 'live',
+      'source-layer': 'construction',
+      paint: {
+        'circle-color': '#000000',
+        'circle-radius': {
+          stops: [[10, 2], [15, 8], [20, 22]]
+        },
+        'circle-opacity': {
+          stops: [[13, 0.3], [zoomStart, 0.5], [20, 1]]
+        }
+      }
+    });
+
+    map.addLayer({
+      id: 'construction-fill',
+      type: 'circle',
+      source: 'live',
+      'source-layer': 'construction',
+      paint: {
+        'circle-color': '#ffbb00',
+        'circle-radius': {
+          stops: [[10, 1], [15, 6], [20, 19]]
+        }
+      }
+    });
+
     //
     // Layers - draw lines, dots, etc on map from source data
     //
 
-    // Uncomment to directly access the OSM data in mapbox - sidewalks!
-    // map.addLayer({
-    //   id: 'sidewalksosm',
-    //   type: 'line',
-    //   source: 'composite',
-    //   'source-layer': 'road',
-    //   filter: ['all', ['==', 'class', 'path'], ['==', 'type', 'sidewalk']],
-    //   paint: {
-    //     'line-color': '#ff00ff',
-    //     'line-width': {
-    //       stops: [[12, 1.5], [clickable, 5], [20, 12]]
-    //     },
-    //     'line-opacity': {
-    //       stops: [[13, 0.2], [clickable, 0.6], [20, 1]]
-    //     }
-    //   },
-    //   layout: {
-    //     'line-cap': 'round'
-    //   }
-    // }, 'bridge-path-bg');
-
     // Crossings
     map.addLayer({
-      id: 'crossings-outline',
+      id: 'crossings-noramps',
       type: 'line',
       source: 'seattle',
       'source-layer': 'crossings',
+      filter: ['!=', 'curbramps', true],
       paint: {
         'line-color': '#000000',
         'line-width': {
-          stops: [[12, 1.5], [clickable, 5], [20, 12]]
+          stops: [[12, 0.5], [16, 2], [20, 20]]
         },
         'line-opacity': {
-          stops: [[13, 0.0], [clickable, 0.1], [20, 0.2]]
+          stops: [[13, 0.0], [zoomStart, 0.05], [20, 0.15]]
         }
       },
       layout: {
@@ -105,27 +125,16 @@ function App(mapbox_token, routing) {
       'source-layer': 'crossings',
       filter: ['==', 'curbramps', true],
       paint: {
-        'line-color': '#222222',
+        'line-color': '#000000',
         'line-width': {
-          stops: [[12, 0.3], [clickable, 1], [20, 6]]
+          stops: [[12, 0.5], [16, 2], [20, 20]]
         },
-        'line-opacity': 0.7
+        'line-opacity': {
+          stops: [[13, 0.0], [zoomStart, 0.3], [20, 0.3]]
+        },
       },
-      minzoom: 14
-    }, 'bridge-path-bg');
-
-    map.addLayer({
-      id: 'crossings-notramps',
-      type: 'line',
-      source: 'seattle',
-      'source-layer': 'crossings',
-      filter: ['==', 'curbramps', false],
-      paint: {
-       'line-color': '#222222',
-        'line-width': {
-          stops: [[12, 0.3], [clickable, 1], [20, 6]]
-        },
-        'line-opacity': 0.2
+      layout: {
+        'line-cap': 'round'
       },
       minzoom: 14
     }, 'bridge-path-bg');
@@ -139,10 +148,13 @@ function App(mapbox_token, routing) {
       paint: {
         'line-color': '#000000',
         'line-width': {
-          stops: [[12, 1.5], [clickable, 5], [20, 12]]
+          stops: [[12, 1], [zoomStart, 1.5], [20, 2]]
         },
         'line-opacity': {
-          stops: [[13, 0.0], [clickable, 0.1], [20, 0.2]]
+          stops: [[13, 0.0], [zoomStart, 0.1], [20, 0.2]]
+        },
+        'line-gap-width': {
+          stops: [[12, 0.5], [16, 3], [20, 30]]
         }
       },
       layout: {
@@ -168,10 +180,10 @@ function App(mapbox_token, routing) {
           ]
         },
         'line-width': {
-          stops: [[12, 0.5], [clickable, 2], [20, 8]]
+          stops: [[12, 0.5], [16, 3], [20, 30]]
         },
         'line-opacity': {
-          stops: [[12, 0.5], [clickable, 1.0], [20, 1.0]]
+          stops: [[12, 0.2], [zoomStart, 0.7], [20, 0.5]]
         }
       },
       layout: {
@@ -277,41 +289,91 @@ function App(mapbox_token, routing) {
         return;
       }
 
-      // Display sidewalk/crossing info
-      let gradePaths = map.queryRenderedFeatures(e.point, {
-        layers: ['sidewalks', 'crossings-ramps', 'crossings-notramps']
+      // TODO: should probably use switch/case after detecting layers
+
+      // If construction is clicked, show that
+      let construction = map.queryRenderedFeatures(e.point, {
+        layers: ['construction-outline']
       });
 
-      if (!gradePaths.length) {
-        return;_
+      if (construction.length) {
+        let selected = construction[0];
+        let props = selected.properties;
+        let closed = props.closed === 'Y' ? 'Yes' : 'No';
+
+        let permitNumber = props.permit_number;
+        let permitsUrl = 'https://data.seattle.gov/resource/hyub-wfuv.json';
+        permitsUrl += '?permit_no_num=' + permitNumber;
+        let permitsMarkup = '<a href="' + permitsUrl + '">' + permitNumber + '</a>';
+
+        let message = '<h4 style="text-align: center;"><b>Construction</b></h4>';
+        message += '<h4><b>Sidewalk closed:</b> ' + closed + '</h4>';
+        message += '<h4><b>Address:</b> ' + props.address + '</h4>';
+        message += '<h4><b>Started:</b> ' + props.start_date + '</h4>';
+        message += '<h4><b>Ends:</b> ' + props.end_date + '</h4>';
+        message += '<h4><b>SDOT Permit:</b> ' + permitsMarkup + '</h4>';
+
+        let popup = new mapboxgl.Popup()
+          .setLngLat(e.lngLat)
+          .setHTML(message)
+          .addTo(map);
+
+        return;
       }
 
-      // Select the first feature - how is order decided? Hopefully it's the
-      // 'top' layer, i.e. most visible.
-      let path = gradePaths[0];
+      // Display sidewalk/crossing info
+      let sidewalks = map.queryRenderedFeatures(e.point, {
+        layers: ['sidewalks']
+      });
 
-      // Prepare the popup message
-      let gradePercent = path.properties.grade * 100;
-      let message = '<h4><b>Grade:</b> ' + gradePercent.toFixed(1) + '%</h4>';
+      if (sidewalks.length) {
+        let path = sidewalks[0];
 
-      let popup = new mapboxgl.Popup()
-        .setLngLat(e.lngLat)
-        .setHTML(message)
-        .addTo(map);
+        let gradePercent = Math.abs((path.properties.grade * 100).toFixed(1));
+        let message = '<h4 style="text-align: center;"><b>Sidewalk</b></h4>';
+        message += '<h4><b>Steepness:</b> ' + gradePercent + '%</h4>';
+
+        let popup = new mapboxgl.Popup()
+          .setLngLat(e.lngLat)
+          .setHTML(message)
+          .addTo(map);
+      }
+
+      let crossings = map.queryRenderedFeatures(e.point, {
+        layers: ['crossings-ramps', 'crossings-noramps']
+      });
+
+      if (crossings.length) {
+        let path = crossings[0];
+
+        let props = path.properties;
+        let gradePercent = Math.abs((props.grade * 100).toFixed(1));
+        let curbRamps = props.curbramps ? 'Yes' : 'No';
+
+        let message = '<h4 style="text-align: center;"><b>Crossing</b></h4>';
+        message += '<h4><b>Steepness:</b> ' + gradePercent + '%</h4>';
+        message += '<h4><b>Curbramps:</b> ' + curbRamps + '</h4>';
+
+        let popup = new mapboxgl.Popup()
+          .setLngLat(e.lngLat)
+          .setHTML(message)
+          .addTo(map);
+      }
     });
 
-    // Make cursor into 'clicker' pointer when hovering over clickable elements
+    // Make cursor into 'clicker' pointer when hovering over zoomStart elements
     map.on('mousemove', function(e) {
       // Only allow clicks at high zoom levels
       if (map.getZoom() < clickable) {
         return;
       }
 
-      let gradePaths = map.queryRenderedFeatures(e.point, {
-        layers: ['sidewalks', 'crossings-ramps', 'crossings-notramps']
+      let clickables = map.queryRenderedFeatures(e.point, {
+        layers: ['sidewalks', 'crossings-ramps', 'crossings-noramps',
+                 'construction-outline']
       });
 
-      map.getCanvas().style.cursor = (gradePaths.length) ? 'pointer': '';
+      map.getCanvas().style.cursor = (clickables.length) ? 'pointer': '';
     });
 
   });
