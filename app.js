9e59f45b00ed14b41f796c1de1e116d083ef9b6b
Nick Bolten <nbolten@gmail.com>
2017-02-23 00:56:04 -0800

    Removed unused login and database code, updated jade to pug

--- app.js
+++ app.js
@@ -1,17 +1,8 @@
 var express = require('express'),
     path = require('path'),
     favicon = require('serve-favicon'),
-    logger = require('morgan'),
-    cookieParser = require('cookie-parser'),
-    bodyParser = require('body-parser');
+    logger = require('morgan');
 
-var session = require('express-session'),
-    flash = require('connect-flash'),
-    passport = require('passport'),
-    bcrypt = require('bcrypt-nodejs'),
-    LocalStrategy = require('passport-local').Strategy;
-
-var models = require('./models');
 var index = require('./routes/index');
 var api = require('./routes/api');
 var tiles = require('./routes/tiles');
@@ -21,115 +12,17 @@ var env = process.env.NODE_ENV || 'development';
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 app.use(favicon(path.join(__dirname, 'public/favicon.ico')));
-if (env == 'production') {
-  var secret = 'productionSecret';
-} else if (env == 'development') {
-  var secret = 'developmentSecret';
-}
-
-// Store session info (automatically) in the database
-var SequelizeStore = require('connect-session-sequelize')(session.Store);
-var store = new SequelizeStore({
-  db: models.sequelize
-});
-
-if (env === 'development') {
-  store.sync();
-}
-
-app.use(session({
-  store: store,
-  secret: secret,
-  resave: false,
-  saveUninitialized: false
-}));
-
-// Set up flash messages (e.g. bad login message at top of page)
-app.use(flash());
-app.use(function(req, res, next) {
-  res.locals.errorMessage = req.flash('error');
-  res.locals.successMessage = req.flash('success');
-  next();
-});
 
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-// Set up testing user if not using production database
-if (env === 'development') {
-  models.user
-    .sync({force: true})
-    .then(function(err) {
-      var salt = bcrypt.genSaltSync(10);
-      models.user.create({
-        username: 'test',
-        email: 'test@test.com',
-        salt: salt,
-        password: bcrypt.hashSync('test', salt)
-      });
-      return 1;
-    }, function(err) {
-      console.log('Error syncing database');
-    });
-}
-
-// Passport (login)
-app.use(passport.initialize());
-app.use(passport.session());
-
-passport.use(new LocalStrategy(
-  function(username, password, done) {
-    models.user.findOne({
-      where: {
-        'username': username
-      }
-    }).then(function (user) {
-      var failed = false;
-      if (user === null) {
-        return done(null, false, { message: 'Incorrect username or pass.' });
-      }
-
-      var hashedPassword = bcrypt.hashSync(password, user.salt);
-
-      if (user.password === hashedPassword) {
-        return done(null, user);
-      }
-
-      return done(null, false, { message: 'Incorrect username or pass.' });
-      });
-    }
-  ));
-
-passport.serializeUser(function(user, done) {
-  done(null, user.id);
-});
-
-passport.deserializeUser(function(id, done) {
-  models.user.findOne({
-    where: {
-      'id': id
-    }
-  }).then(function (user) {
-    if (user === null) {
-      done(new Error('Wrong user id.'));
-    }
-
-    done(null, user);
-  });
-});
 
 // Routes
 app.use('/', index);
 app.use('/api', api);
 app.use('/tiles', tiles);
-// SSL - specific to Azure ACME (letsencrypt) plugin
-app.use('/.well-known', express.static('.well-known'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,6 @@
-var bcrypt = require('bcrypt-nodejs'),
-    express = require('express'),
-    passport = require('passport'),
+var express = require('express'),
     path = require('path'),
     request = require('request');
-
-var models = require('../models');
 
 var router = express.Router();
 
@@ -15,8 +11,7 @@ var router = express.Router();
 // GET index page
 router.get('/', function(req, res) {
   res.render('index', {
-    mapbox_token: JSON.stringify(process.env.MAPBOX_TOKEN),
-    user: req.user
+    mapbox_token: JSON.stringify(process.env.MAPBOX_TOKEN)
   });
 });
 
@@ -26,128 +21,17 @@ router.get('/', function(req, res) {
 
 // GET index page
 router.get('/about', function(req, res) {
-  res.render('about', { user: req.user });
-});
-
-//
-// Registration and login
-//
-
-function isAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) {
-    return next();
-  }
-
-  req.session.returnTo = req.path;
-  req.flash('error', 'You have to be logged in to access this page.');
-  res.redirect('/login');
-}
-
-// GET comingsoon
-router.get('/comingsoon', function(req, res) {
-  res.render('comingsoon');
-});
-
-// GET register
-router.get('/register', function(req, res) {
-  res.redirect('/comingsoon');
-  res.render('register');
-});
-
-// POST register
-router.post('/register', function(req, res) {
-  var username = req.body.username;
-  var email = req.body.email;
-  var password = req.body.password;
-
-  if (!username || !email || !password) {
-    req.flash('error', 'Required field missing.');
-    res.redirect('/register');
-  } else {
-    var salt = bcrypt.genSaltSync(10);
-    var hashedPassword = bcrypt.hashSync(password, salt);
-
-    var newUser = {
-      username: username,
-      email: email,
-      salt: salt,
-      password: hashedPassword
-    };
-
-    models.User.create(newUser).then(function() {
-      res.redirect('/login');
-    }).catch(function(error) {
-      console.log(error);
-      res.redirect('/');
-    });
-  }
-});
-
-// GET login page
-router.get('/login', function(req, res) {
-  // TODO: Remove the redirect to re-enable login page
-  res.redirect('/comingsoon');
-  res.render('login', { user: req.user });
-});
-
-// POST login page
-router.post('/login', passport.authenticate('local', {
-  successReturnToOrRedirect: '/',
-  failureRedirect: '/login',
-  failureFlash: true
-}));
-
-// GET logout page
-router.get('/logout', function(req, res) {
-  res.redirect('/comingsoon');
-  req.logout();
-  req.flash('success', 'You have successfully logged out.');
-  res.redirect('/');
-});
-
-// GET profile page
-router.get('/profile', function(req, res) {
-  res.redirect('/comingsoon');
-  res.render('profile', { user: req.user });
+  res.render('about');
 });
 
 //
 // Map pages
 //
 
-// GET map page
-router.get('/map', function(req, res) {
-  res.render('map', {
-    mapbox_token: JSON.stringify(process.env.MAPBOX_TOKEN),
-    user: req.user
-  });
-});
-
 router.get('/isochrones', function(req, res) {
   res.render('isochrones', {
     mapbox_token: JSON.stringify(process.env.MAPBOX_TOKEN),
     user: req.user
-  });
-});
-
-router.get('/improve', function(req, res) {
-  res.render('improve', { user: req.user });
-});
-
-router.get('/learnsidewalks', isAuthenticated, function(req, res, next) {
-  res.render('learnsidewalks', {
-    learn_url: JSON.stringify(process.env.LEARN_URL),
-    user: req.user.username
-  });
-});
-
-router.get('/getdata', isAuthenticated, function(req, res, next) {
-  request(process.env.LEARN_URL + '/getdata' + '?user=' + req.user.username, function(e, r, b) {
-    if (e) {
-      res.send('Error: ' + e);
-    } else {
-      res.send(b);
-    }
   });
 });
 
